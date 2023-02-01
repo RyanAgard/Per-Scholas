1f4aea780cab6fc153030ed3c7582ebb8a57e11d
Ryan Agard <ryantagard@yahoo.com>
2023-02-01 14:45:26 -0500

    Big changes

--- Mod 1 - Game On!/app.js
+++ Mod 1 - Game On!/app.js
@@ -1,8 +1,14 @@
 const grid = document.querySelector(".grid")
+const scoreDisplay = document.querySelector('#score')
 const blockwidth = 100
 const blockheight = 20
 const boardWidth = 560
-
+const boardHeight =300
+const ballDiameter = 20
+let timerId 
+let xDirection = 2
+let yDirection = 2
+let score = 0
 
 
 const userStart = [230,10]
@@ -10,6 +16,11 @@ let currentPosition = userStart
 
 const ballStart = [270,40]
 let ballCurrentPosition = ballStart
+
+
+
+
+
 //block maker 
 class Block {
  constructor(xAis,yAxis){
@@ -51,7 +62,7 @@ function moreBlocks(){
   }
 }
  moreBlocks()
- //User
+ //userCreated
  const user = document.createElement('div')
  user.classList.add('user')
  relocate()
@@ -61,16 +72,17 @@ function moreBlocks(){
 
 //relocate
 function relocate(){
- user.style.left = currentPosition[0]+'px'
- user.style.bottom = currentPosition[1]+'px'
+  user.style.left = currentPosition[0]+'px'
+  user.style.bottom = currentPosition[1]+'px'
 }
 function Ballrelocate(){
-  ballCurrentPosition[1]+'px'
-  ballCurrentPosition[0]+'px'
+  ball.style.left=ballCurrentPosition[0]+'px'
+  ball.style.bottom=ballCurrentPosition[1]+'px'
+  
 }
 
-//movement
-function movement(e){
+//player1Movement
+function moveMent(e){
    switch(e.key){
   case'ArrowLeft':
     if(currentPosition[0] >0){
@@ -86,21 +98,86 @@ function movement(e){
    break;
    }
 }
-document.addEventListener('keydown',movement)
+document.addEventListener('keydown',moveMent)
 
-//ball
+//createdBall
 const ball = document.createElement('div')
-ball.classList.add('ball')
-ball.style.left = ballCurrentPosition[0]+'px'
-ball.style.bottom = ballCurrentPosition[1]+'px'
+ ball.classList.add('ball')
+ ball.style.left = ballCurrentPosition[0]+'px'
+ ball.style.bottom = ballCurrentPosition[1]+'px'
 grid.appendChild(ball)
  
 
-
-//ball Movement
+//ballMovement
 function moveBall(){
-ballCurrentPosition[0] +=2
-ballCurrentPosition[1] +=2
-Ballrelocate()
-
+ballCurrentPosition[0] +=xDirection
+ballCurrentPosition[1] += yDirection
+ Ballrelocate()
+ checkForWall()
 }
+timerId=setInterval(moveBall,30)
+
+
+function checkForWall(){
+  // check for block collisions
+   for(let i= 0; i< blocks.length; i++){
+    if(
+      (ballCurrentPosition[0] > blocks[i].bottomLeft[0] && ballCurrentPosition[0] < blocks[i].bottomRight[0]) &&
+      ((ballCurrentPosition[1] + ballDiameter) > blocks[i].bottomLeft[1] && ballCurrentPosition[1] < blocks[i].topleft[1])
+    ){
+    const allBlocks = Array.from(document.querySelectorAll('.block'))
+    allBlocks[i].classList.remove('block')
+    blocks.splice(i, 1)
+    changeDirection()
+    score++
+    scoreDisplay.innerHTMl= score
+ //check for win
+    if (blocks.length===0){
+      scoreDisplay.innerHTML='YOU WIN'
+      clearInterval(timerId)
+      document.removeEventListener('keydown',moveMent)
+     }
+  }
+} 
+  //  check for wall collisions
+  if (ballCurrentPosition[0] >= (boardWidth - ballDiameter) || 
+      ballCurrentPosition[1] >= (boardHeight - ballDiameter) ||
+      ballCurrentPosition[0] <= 0
+      ){  
+        changeDirection()
+      }
+ //check for player collisions
+    if(
+      (ballCurrentPosition[0] > currentPosition[0] && ballCurrentPosition[0]< currentPosition[0] + blockwidth) && 
+      (ballCurrentPosition[1] > currentPosition[1] && ballCurrentPosition[1]< currentPosition[1] + blockheight))
+     {
+      changeDirection()
+    }
+  
+  //check for game over 
+  if(ballCurrentPosition[1] <= 0){
+    clearInterval(timerId)
+    scoreDisplay.innerHTML= 'You Lose'
+    document.removeEventListener('keydown',moveMent)
+  }  
+}
+
+//check for walls
+ function changeDirection(){
+     if (xDirection === 2 && yDirection === 2){
+         yDirection =-2
+         return
+     }
+     if(xDirection === 2 && yDirection === -2){
+        xDirection =-2
+        return
+     }
+     if (xDirection === -2 && yDirection === -2){
+        yDirection = 2
+        return
+     }
+     if(xDirection === -2 && yDirection === 2){
+       xDirection = 2
+      return
+     }
+ }
